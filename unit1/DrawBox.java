5b055eb9bc476a37ad4c27a1423dcd5f3b1f3ad5
Scottie Enriquez <scott.c.enriquez@gmail.com>
2019-09-11 20:07:42 -0500

    Adding DrawBox program

---
+++ unit1/DrawBox.java
@@ -0,0 +1,18 @@
+public class DrawBox 
+{
+    public static void main (String[] args) {
+        drawBox();
+        drawBox();
+        drawBox();
+
+    }
+
+    public static void drawBox() {
+        System.out.println("xxxxxxxxx");
+        System.out.println("x       x");
+        System.out.println("x       x");
+        System.out.println("x       x");
+        System.out.println("x       x");
+        System.out.println("xxxxxxxxx");
+    }
+}
